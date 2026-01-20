4bf9635cb08e1edfea57a156b76e739202b42c33
giovanaluizapereira <111535336+giovanaluizapereira@users.noreply.github.com>
2026-01-19 21:19:44 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,10 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-// No Vercel, estas variáveis são injetadas automaticamente no process.env
 const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
 
-// Inicializamos o cliente. Se as chaves forem vazias, o cliente será criado mas as chamadas falharão com erro descritivo.
+// Inicializamos o cliente.
 export const supabase = (supabaseUrl && supabaseAnonKey) 
   ? createClient(supabaseUrl, supabaseAnonKey)
   : null;
